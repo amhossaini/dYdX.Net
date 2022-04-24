b20dd7bee7357217fc995931afbb6dcc1da12db2
amhossaini <90958612+amhossaini@users.noreply.github.com>
2022-04-24 19:25:23 +0430

    Update sign.py

--- dYdX.Net/StarkexHelper/sign.py
+++ dYdX.Net/StarkexHelper/sign.py
@@ -7,39 +7,6 @@ import dateutil.parser as dp
 import sys
 import json
 
-# print('Number of arguments:', len(sys.argv), 'arguments.')
-# print('Argument List:', str(sys.argv))
-
-# obj = sys.argv[1]
-# print(obj)
-# obj = '{"side" : "BUY"}'
-# y = json.loads(obj)
-# print(y["side"])
-
-
-# d = datetime.now()
-
-# print(d.strftime(
-#         '%Y-%m-%dT%H:%M:%S.%f',
-#     )[:-3] + 'Z')
-
-# ts = dp.parse(d.strftime(
-#         '%Y-%m-%dT%H:%M:%S.%f',
-#     )[:-3] + 'Z').timestamp()
-# print(ts)
-
-# order_to_sign = SignableOrder(
-#                 network_id=1,
-#                 position_id='2323',
-#                 client_id='987676',
-#                 market='BTC-USD',
-#                 side='BUY',
-#                 human_size='1.1',
-#                 human_price='43000',
-#                 limit_fee='100000',
-#                 expiration_epoch_seconds=1548664452.999,
-#             )
-# print(dp.parse(sys.argv[9]).timestamp())
 order_to_sign = SignableOrder(
                 network_id=int(sys.argv[1]),
                 position_id=sys.argv[2],
